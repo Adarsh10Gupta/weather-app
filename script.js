40fd727520af6604aa8f6518db26b2a15cd70b43
Adarsh10Gupta <adarsh10gupta@gmail.com>
2024-09-15 20:34:54 +0530

    users_climate

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ let weather = {
         document.querySelector(".city").innerText = "Weather in "+ name;
         document.querySelector(".temp").innerText = temp + "°C";
         document.querySelector(".weather_icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
-        document.querySelector(".description").innerText = description;
+        document.querySelector(".description").innerText = "Description: " + description;
         document.querySelector(".humidity").innerText = "Humidity: "+ humidity + "%";
         document.querySelector(".wind").innerText = "Wind Speed: "+ speed+ "km/h";
 
@@ -54,3 +54,48 @@ document.querySelector(".search-bar").addEventListener("keyup",function(event){
 });
 
 weather.fetchweather("kolkata");
+
+
+//Getting the current location of the user
+
+const button = document.getElementById('get-location');
+const user_box = document.getElementById('user');
+
+function gotlocation(position){
+    console.log(position);
+    const lat = position.coords.latitude;
+    const long = position.coords.longitude;
+    use_cord(lat,long);
+
+}
+function failedToGet(){
+    console.log("There was some issue");
+}
+button.addEventListener('click', async () => {
+    button.remove();
+    user_box.remove();
+    navigator.geolocation.getCurrentPosition(gotlocation,failedToGet)
+});
+function use_cord(lat,long){
+    api = "f5450408b13af0a1ec215c4d18eec4f6",
+    fetch("https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+long+"&appid="+api+ "&units=metric")
+    .then(response => response.json())
+    .then(data => {
+        console.log("weather data: ",data);
+
+    const {name} = data;
+    const {temp, humidity} = data.main;
+    const {icon, description} = data.weather[0];
+    const {speed} = data.wind; 
+
+    document.querySelector(".user_city").innerText = "Weather in "+ name;
+    document.querySelector(".user_temp").innerText = temp + "°C";
+    document.querySelector(".user_weather_icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
+    document.querySelector(".user_des").innerText = "Description: " + description;
+    document.querySelector(".user_humi").innerText = "Humidity: "+ humidity + "%";
+    document.querySelector(".user_wind").innerText = "Wind Speed: "+ speed+ "km/h";
+})
+.catch(error => {
+    console.error("Error fetching weather: ",error);
+});
+}
